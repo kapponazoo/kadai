ec54d2f766c9758f86613c4db5df1dc6358d2cf4
kapponazoo <kapponazoo@gmail.com>
2024-03-01 23:07:06 +0900

    へドルと無地作成

--- js/color_set.js
+++ js/color_set.js
@@ -2,37 +2,37 @@ const colorSet = [
     {
         "name": "藍色",
         "rub": "あいいろ",
-        "pref": "#165e83"
+        "colorcode": "#165e83"
     },
     {
         "name": "百入茶",
         "rub": "ももしおちゃ",
-        "pref": "#1f3134"
+        "colorcode": "#1f3134"
     },
     {
         "name": "赤墨",
         "rub": "あかすみ",
-        "pref": "#3f312b"
+        "colorcode": "#3f312b"
     },
     {
         "name": "藍海松茶",
         "rub": "あいみるちゃ",
-        "pref": "#56564b"
+        "colorcode": "#56564b"
     },
     {
         "name": "香染",
         "rub": "こうぞめ",
-        "pref": "#ad7d4c"
+        "colorcode": "#ad7d4c"
     },
     {
         "name": "浅紫",
         "rub": "あさむらさき",
-        "pref": "#ad7d4c"
+        "colorcode": "#ad7d4c"
     },
     {
         "name": "練色",
         "rub": "ねりいろ",
-        "pref": "#ede4cd"
+        "colorcode": "#ede4cd"
     },
     {
         "name": "甚三紅",
@@ -42,7 +42,7 @@ const colorSet = [
     {
         "name": "萱草色",
         "rub": "かんぞういろ",
-        "pref": "#f8b862"
+        "colorcode": "#f8b862"
     }
 
 ];

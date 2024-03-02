6edd5e7a1e5cf8a0441bbf827ffaae7088f69663
kapponazoo <kapponazoo@gmail.com>
2024-03-03 00:22:36 +0900

    模様入り

---
+++ js/color_set_warm.js
@@ -0,0 +1,48 @@
+const colorSet = [
+    {
+        "name": "長春色",
+        "rub": "ちょうしゅんいろ",
+        "colorcode": "#c97586"
+    },
+    {
+        "name": "梅鼠",
+        "rub": "うめねず",
+        "colorcode": "#c099a0"
+    },
+    {
+        "name": "鴇浅葱",
+        "rub": "ときあさぎ",
+        "colorcode": "#b88884"
+    },
+    {
+        "name": "梅染",
+        "rub": "うめぞめ",
+        "colorcode": "#b48a76"
+    },
+    {
+        "name": "蘇芳香",
+        "rub": "すおうこう",
+        "colorcode": "#a86965"
+    },
+    {
+        "name": "榛色",
+        "rub": "はしばみいろ",
+        "colorcode": "#bfa46f"
+    },
+    {
+        "name": "亜麻色",
+        "rub": "あまいろ",
+        "colorcode": "#d6c6af"
+    },
+    {
+        "name": "芥子色",
+        "rub": " からしいろ",
+        "colorcode": "#d0af4c"
+    },
+    {
+        "name": "鳶色",
+        "rub": "とびいろ",
+        "colorcode": "#95483f"
+    }
+
+];
